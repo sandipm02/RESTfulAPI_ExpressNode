de9f34fc0f7bc99a49b2b644fd8a8662eeb132ac
Sandip Mishra <sandipm02@icloud.com>
2021-05-25 10:30:05 -0600

    added dependecies, created basic REST

---
+++ server/api/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const { Prohairesis } = require('prohairesis');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+app
+    .use(express.static('../../client'))
+    .use(morgan('dev'))
+
+    .use(bodyParser.urlencoded({extended: false}))
+    .use(bodyParser.json())
+
+    .listen(port, () => console.log('Server Listening on port ' + port));
