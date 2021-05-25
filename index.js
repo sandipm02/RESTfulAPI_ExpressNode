192bbc963375a5914c19336731f3055ece680e70
Sandip Mishra <sandipm02@icloud.com>
2021-05-25 12:54:14 -0600

    POST working

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const { Prohairesis } = require('prohairesis');
 const bodyParser = require('body-parser');
+const { response } = require('express');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-
 app
-    .use(express.static('public'))
     .use(morgan('dev'))
-
+    .use(express.static('public'))
     .use(bodyParser.urlencoded({extended: false}))
     .use(bodyParser.json())
+
+    .post('/api/user', (req,res)=> {
+        res.json(req.body);
+    })
 
     .listen(port, () => console.log('Server Listening on port ' + port));
 
